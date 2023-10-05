eddf30643b3cd561cde5d102398cc06a381a4048
cosmin <gcboeriu@gmail.com>
2023-10-05 13:57:28 +0300

    push1laptop

--- point.h
+++ point.h
@@ -6,16 +6,21 @@ class Vector{
 private:
     std::vector<double>components;
 public:
-    explicit Vector(unsigned long long size){
+    explicit Vector(unsigned long long size){  // Initializes a Vector full of 0's of size
         for(int i = 0; i < size; i++)
             components.push_back(0);
     }
-    explicit Vector( const std::vector<double>&v){
+    explicit Vector( const std::vector<double>&v){  // Takes a std::vector and initializes a Vector with said elements
         for(auto i : v){
             components.push_back(i);
         }
     }
-    Vector& operator=(const Vector& other){
+    Vector(const Vector& other){
+        components.clear();
+        for(double i : other.components)
+            components.push_back(i);
+    }
+    Vector& operator=(const Vector& other){  // Assigment operator
         if(&other == this){
             return *this;
         }
@@ -38,18 +43,18 @@ public:
             this->components[i] = v.components[i];
         return v;
     }
-    double& operator[](unsigned long long index){
+    double& operator[](unsigned long long index){  // Returns a reference to components[index]
         if(index > components.size())
             throw std::invalid_argument("Array index out of bounds");
         return components[index];
     }
-    double length() const {
+    double length() const {  // Returns modulus of vector
         double sum = 0;
         for(double elem : components)
             sum += elem * elem;
         return sqrt(sum);
     }
-    double dot_product(const Vector& other ) const {
+    double dot_product(const Vector& other ) const {  // Returns the dot product of this object's components and argument
         double sum = 0;
         if(other.components.size() != this->components.size())
             throw std::invalid_argument("Sizes of Vectors are not equal");
@@ -65,10 +70,10 @@ public:
             sum += components[i] * v[i];
         return sum;
     }
-    double calculate_angle(const Vector& other) const{
+    double calculate_angle(const Vector& other) const{  // Calculates the angle between this vector and argument
         return this->dot_product(other) / (this->length() * other.length());
     }
-    unsigned long long get_size(){
+    unsigned long long get_size(){  // Returns the size of vector
         return components.size();
     }
 
@@ -77,19 +82,15 @@ public:
 
 class Matrix{
 private:
-    std::vector<std::vector<double>>mat;
+    std::vector<Vector>mat; // Matrix that has size1 lines and size2 columns. We can assume that Vectors compose the lines
     unsigned long long size1, size2;
 public:
     Matrix(unsigned long long size1, unsigned long long size2): size1(size1), size2(size2){
         for(int i = 0; i < size1; i++){
-            std::vector<double>v1;
-            for(int j = 0; j < size2; j++ ){
-                v1.push_back(0);
-            }
-            mat.push_back(v1);
+            mat.push_back(Vector(size2));
         }
     }
-    explicit Matrix(double m[4][4]){  //Most used matrix deserves its own initialisation
+    explicit Matrix(const std::vector<std::vector>&v){
         size1 = size2 = 4;
         for(int i = 0; i < size1; i++){
             std::vector<double>v1;
@@ -101,6 +102,8 @@ public:
     }
     Vector& operator*(const Vector& v) const{
         Vector rezultat = Vector(4);
+
     }
+
 };
 
